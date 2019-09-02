7f1ccd3af81b9802d258a31de4b14ee4a04ecaa1
UNC Support <UNC Support@192.168.1.82>
2019-09-01 22:55:37 -0400

    Novice Completed v1

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,32 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
+		
 		// Your code follows here.
 		
+		int customerTotal = scan.nextInt();
+		
+		// Holds the customer total
+		
+		for (int i = 0; i < customerTotal; i++) {
+			String firstName = scan.next();
+			char fi = firstName.charAt(0);
+			String lastName = scan.next();
+			int itemCount = scan.nextInt();
+			double total = 0;
+			
+			// for loop to get the information on each customer
+			
+			for (int j = 0; j < itemCount; j++) {
+				int many = scan.nextInt();
+				String name = scan.next();
+				// Not needed but I don't know how to skip a token
+				double price = scan.nextDouble();
+				double totalPrice = price * many;
+				total += totalPrice;
+			}
+			System.out.println(fi + ". " + lastName + ": " + total);
+		}
+		scan.close();
 	}
 }
